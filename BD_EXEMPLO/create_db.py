6e05b9b9e02a4b51b0137289cd392c44805342fc
HyagoerS <hyagossoficiall@gmail.com>
2026-03-31 17:15:10 +0000

    atributos inseridos

--- BD_EXEMPLO/create_db.py
+++ BD_EXEMPLO/create_db.py
@@ -1,8 +1,28 @@
+from requests import Session
 from sqlalchemy import create_engine
 from models import Base
+from sqlalchemy.orm import sessionmaker
+from models import Curso, Estudante
 
-engine = create_engine("sqlite:///escola.db", echo=True)
 
-print("Gerando o schema do banco de dados...")
+
+engine = create_engine("sqlite:///escola.db")
+#engine = create_engine("sqlite:///escola.db", echo=True)
 Base.metadata.create_all(engine)
-print("Schema gerado com sucesso!")
+Session = sessionmaker(bind=engine)
+
+with Session() as session:
+    curso_eng_comp = Curso(nome="Engenharia de Computação")
+    estudante_ana = Estudante(nome="Ana", email="ana@email.com", curso_id=1)
+    print("DSebug")
+    session.add(curso_eng_comp)
+    session.add(estudante_ana)
+
+    session.commit()
+
+
+
+
+#print("Gerando o schema do banco de dados...")
+
+#print("Schema gerado com sucesso!")

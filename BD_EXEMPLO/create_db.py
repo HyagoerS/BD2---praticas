42c518ef1930741ce6c1675a4e8914f07c7aa627
HyagoerS <hyagossoficiall@gmail.com>
2026-03-31 17:18:32 +0000

    venv excluido

--- BD_EXEMPLO/create_db.py
+++ BD_EXEMPLO/create_db.py
@@ -1,6 +1,6 @@
 from requests import Session
 from sqlalchemy import create_engine
-from models import Base
+#from models import Base
 from sqlalchemy.orm import sessionmaker
 from models import Curso, Estudante
 
@@ -8,13 +8,12 @@ from models import Curso, Estudante
 
 engine = create_engine("sqlite:///escola.db")
 #engine = create_engine("sqlite:///escola.db", echo=True)
-Base.metadata.create_all(engine)
+#Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 with Session() as session:
     curso_eng_comp = Curso(nome="Engenharia de Computação")
     estudante_ana = Estudante(nome="Ana", email="ana@email.com", curso_id=1)
-    print("DSebug")
     session.add(curso_eng_comp)
     session.add(estudante_ana)
 
